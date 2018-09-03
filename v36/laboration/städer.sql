6380848ea23f054a93411563d9d3fcbd96fea7d4
JaoK <JJaoKK@gmail.com>
2018-09-03 12:16:33 +0200

    Create städer.sql

---
+++ v36/laboration/städer.sql
@@ -0,0 +1,28 @@
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Alingsås","Allingsås kommun","Västergötland","Västra Götalands län",44100,24000);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Arboga","Arboga kommun","Västmanland","Västmanlands län",73201,10330);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Arvika","Arvika kommun","Värmland","Värmlands län",67101,14244);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Askersund","Askersunds kommun","Närke","Örebro län",69601,3887);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Avesta","Avesta kommun","Dalarna","Dalarnas län",77401,14506);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Boden","Bodens kommun","Norrbotten","Norrbottens län",96100,18277);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Bollnäs","Bollnäs kommun","Hälsingland","Gävleborgs län",82100,12842);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Borgholm","Borgholms kommun","Öland","Kalmar län",38701,3071);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Borlänge","Borlänge kommun","Dalarna","Dalarnas län",78100,41734);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Borås","Borås kommun","Västergötland","Västra Götalands län",50100,66273);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Båstad","Båstad kommun","Skåne","Skåne län",26901,4961);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Enköping","Enköpings kommun","Uppland","Uppsala län",74500,21121);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Eskilstuna","Eskilstuna kommun","Södermanland","Södermanlands län",63003,64679);
+INSERT INTO städer(stad,kommun,landskap,län,postkod,invånare) VALUES  
+("Eslöv","Eslöv kommun","Skåne","Skåne län",24110,17748);
